27250a414b539892dbe73b4b3982a2e398d921ac
Tintin-wky <760520446@qq.com>
2023-12-01 22:54:34 +0800

    add sudo

--- deployment/src/gnm_classbot.sh
+++ deployment/src/gnm_classbot.sh
@@ -3,6 +3,7 @@
 # Create a new tmux session
 session_name="gnm_classnpt_$(date +%s)"
 tmux new-session -d -s $session_name
+pass_word="classlab"
 
 # Split the window into two panes
 tmux selectp -t 0    # select the first (0) pane
@@ -10,7 +11,7 @@ tmux splitw -v -p 50 # split it into two halves
 
 # Run the can0 connect command in the first pane
 tmux select-pane -t 0
-tmux send-keys "modprobe gs_usb" Enter
+tmux send-keys "echo ${pass_word} | modprobe gs_usb" Enter
 tmux send-keys "ip link set can0 up type can bitrate 500000" Enter
 tmux send-keys "candump can0" Enter
 
