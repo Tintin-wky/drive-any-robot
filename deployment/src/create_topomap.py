62cd2551cd21f2a83996d8fad2463f0b8b3c0e19
Tintin-wky <760520446@qq.com>
2023-12-05 15:04:18 +0800

    change create_topomap

--- deployment/src/create_topomap.py
+++ deployment/src/create_topomap.py
@@ -1,21 +1,16 @@
-import argparse
-import os
-from utils import msg_to_pil 
-import time
-import shutil
-
-# ROS
 import rospy
-from sensor_msgs.msg import Image
-
-from topomap import Topomap
+import rosbag
+import argparse
+import shutil
+import os
 import pickle
+from topomap import Topomap
+from PIL import Image
+import io
 
-IMAGE_TOPIC = "/camera/left/image_raw"
+IMAGE_TOPIC = "/camera/left/image_raw/compressed"
 TOPOMAP_IMAGES_DIR = "../topomaps/images"
 TOPOMAP_MATRIX = "../topomaps/matrix.pkl"
-obs_img = None
-
 
 def remove_files_in_dir(dir_path: str):
     for f in os.listdir(dir_path):
@@ -28,28 +23,8 @@ def remove_files_in_dir(dir_path: str):
         except Exception as e:
             print("Failed to delete %s. Reason: %s" % (file_path, e))
 
-def not_green_image(image):
-    green_pixels = 0
-    total_pixels = image.width * image.height
-
-    for pixel in image.getdata():
-        if pixel[0] == 0 and pixel[1] != 0 and pixel[2] == 0:  # (R, G, B)，检查是否为绿色
-            green_pixels += 1
-
-    # 如果所有像素都是绿色，则图像是完全绿色的
-    return green_pixels != total_pixels
-
-def callback_obs(msg: Image):
-    global obs_img
-    temp_img = msg_to_pil(msg)
-    if not_green_image(temp_img):
-        obs_img = temp_img
-
 def main(args: argparse.Namespace):
-    global obs_img
     rospy.init_node("CREATE_TOPOMAP", anonymous=False)
-    image_curr_msg = rospy.Subscriber(
-        IMAGE_TOPIC, Image, callback_obs, queue_size=1)
 
     topomap_name_dir = os.path.join(TOPOMAP_IMAGES_DIR, args.name)
     if not os.path.isdir(topomap_name_dir):
@@ -57,33 +32,38 @@ def main(args: argparse.Namespace):
     else:
         print(f"{topomap_name_dir} already exists. Removing previous images...")
         remove_files_in_dir(topomap_name_dir)
-    
 
-    assert args.dt > 0, "dt must be positive"
-    rate = rospy.Rate(1/args.dt)
-    print("Registered with master node. Waiting for images...")
+    bag = rosbag.Bag(args.rosbag)
     i = 0
     start_time = float("inf")
     topomap=Topomap()
-    while not rospy.is_shutdown():
-        if obs_img is not None:
+    for topic, msg, t in bag.read_messages(topics=[IMAGE_TOPIC]):
+        if t.to_sec() < start_time:
+            start_time = t.to_sec()
+        if t.to_sec() - start_time >= args.dt:
+            start_time = t.to_sec()
+            obs_img = Image.open(io.BytesIO(msg.data))
             obs_img.save(os.path.join(topomap_name_dir, f"{i}.png"))
             topomap.update(i,obs_img,args.dt)
-            print("published image", i)
-            i += 1
-            rate.sleep()
-            start_time = time.time()
-            obs_img = None
-        if time.time() - start_time > 2 * args.dt:
-            print(f"Topic {IMAGE_TOPIC} not publishing anymore. Shutting down...")
-            rospy.signal_shutdown("shutdown")
-            with open(TOPOMAP_MATRIX, 'wb') as file:
-                pickle.dump(dict([(args.name,topomap.get_adjacency_matrix())]), file)
+            i+=1
+            print("saved image", i)
 
+    print(f"Topic {IMAGE_TOPIC} not publishing anymore. Shutting down...")
+    rospy.signal_shutdown("shutdown")
+    with open(TOPOMAP_MATRIX, 'wb') as file:
+        pickle.dump(dict([(args.name,topomap.get_adjacency_matrix())]), file)
+    bag.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description=f"Code to generate topomaps from the {IMAGE_TOPIC} topic"
+    )
+    parser.add_argument(
+        "--rosbag",
+        "-r",
+        default="../topomaps/bags/test.bag",
+        type=str,
+        help="path to your rosbag (default: topomap)",
     )
     parser.add_argument(
         "--name",
--- deployment/src/create_topomap.sh
+++ deployment/src/create_topomap.sh
@@ -7,9 +7,6 @@ tmux new-session -d -s $session_name
 # Split the window into three panes
 tmux selectp -t 0    # select the first (0) pane
 tmux splitw -v -p 50 # split it into two halves
-tmux splitw -h -p 50 # split it into two halves
-tmux selectp -t 0    # go back to the first pane
-tmux splitw -h -p 50 # split it into two halves
 
 # Run roscore in the first pane
 tmux select-pane -t 0
@@ -18,18 +15,8 @@ tmux send-keys "roscore" Enter
 # Run the create_topoplan.py script with command line args in the second pane
 tmux select-pane -t 1
 tmux send-keys "conda activate gnm_deployment" Enter
-tmux send-keys "python create_topomap.py --dt 1 --name $1" Enter
+tmux send-keys "python create_topomap.py --name $1 --rosbag $2" Enter
 tmux send-keys "python topomap_visualization.py --name $1" Enter
-
-# Image conversion
-tmux select-pane -t 2
-tmux send-keys "rosrun image_transport republish compressed in:=/camera/left/image_raw raw out:=/camera/left/image_raw" Enter
-
-# Change the directory to ../topomaps/bags and run the rosbag play command in the third pane
-tmux select-pane -t 3
-# tmux send-keys "mkdir -p ../topomaps/bags" Enter
-# tmux send-keys "cd ../topomaps/bags" Enter
-tmux send-keys "rosbag play -s 1 -r 5 $2" # feel free to change the playback rate to change the edge length in the graph
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
