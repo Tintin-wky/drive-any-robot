02b9971bf60ff392f454917f9b76fc2a329934ef
Tintin-wky <760520446@qq.com>
2023-12-17 17:17:19 +0800

    change sh

--- deployment/src/record_bag.sh
+++ deployment/src/record_bag.sh
@@ -5,18 +5,8 @@ session_name="record_bag_$(date +%s)"
 tmux new-session -d -s $session_name
 pass_word="classlab"
 
-# Split the window into three panes
-tmux selectp -t 0    # select the first (0) pane
-tmux splitw -v -p 50 # split it into two halves
-
-# Run the launch command in the first pane
+# Change the directory to ../topomaps/bags and run the rosbag record command
 tmux select-pane -t 0
-tmux send-keys "echo ${pass_word} | sudo -S modprobe gs_usb" Enter
-tmux send-keys "echo ${pass_word} | sudo -S ip link set can0 up type can bitrate 500000" Enter
-tmux send-keys "roslaunch gnm_classbot.launch" Enter
-
-# Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
-tmux select-pane -t 1
 tmux send-keys "cd ../topomaps/bags/raw" Enter
 tmux send-keys "rosbag record /camera/left/image_raw/compressed /camera/right/image_raw/compressed /scout_status /odom_chassis -o $1" # change topic if necessary
 
